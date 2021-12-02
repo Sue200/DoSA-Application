a5e9015f522b1e858e9b07120e303d745cb1ec20
Suhaila <83363422+Sue200@users.noreply.github.com>
2021-12-02 13:19:07 +0600

    Update EducationChangeRequestBoxController.java

--- src/MainApp/EducationChangeRequestBoxController.java
+++ src/MainApp/EducationChangeRequestBoxController.java
@@ -22,11 +22,7 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-/**
- * FXML Controller class
- *
- * @author Farha
- */
+
 public class EducationChangeRequestBoxController implements Initializable {
 
     @FXML
