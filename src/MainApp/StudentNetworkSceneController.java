70d3310ea0b346ff5acdd83913b1e0b61ecdc1a4
Suhaila <83363422+Sue200@users.noreply.github.com>
2021-12-02 14:21:34 +0600

    Update StudentNetworkSceneController.java

--- src/MainApp/StudentNetworkSceneController.java
+++ src/MainApp/StudentNetworkSceneController.java
@@ -21,11 +21,7 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-/**
- * FXML Controller class
- *
- * @author Farha
- */
+
 public class StudentNetworkSceneController implements Initializable {
 
     @FXML
