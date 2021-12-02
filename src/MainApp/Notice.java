f3523fafb72ec26fcb69e9b8e35b1f93d18bcabc
Suhaila <83363422+Sue200@users.noreply.github.com>
2021-12-02 14:18:26 +0600

    Update Notice.java

--- src/MainApp/Notice.java
+++ src/MainApp/Notice.java
@@ -15,10 +15,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.beans.property.SimpleStringProperty;
 
-/**
- *
- * @author Farha
- */
+
 public class Notice implements Serializable {
     
   private String announcement;
