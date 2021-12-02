4d5033c09ea8765f7f0d92d4651e944f4dc83103
Suhaila <83363422+Sue200@users.noreply.github.com>
2021-12-02 14:24:31 +0600

    Update ViewProfileSceneController.java

--- src/MainApp/ViewProfileSceneController.java
+++ src/MainApp/ViewProfileSceneController.java
@@ -9,11 +9,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 
-/**
- * FXML Controller class
- *
- * @author Farha
- */
+
 public class ViewProfileSceneController implements Initializable {
 
     /**
