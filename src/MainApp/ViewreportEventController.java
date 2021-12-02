6fc2d25850a5eb5af7f16e7198c9cde73a8253a8
Suhaila <83363422+Sue200@users.noreply.github.com>
2021-12-02 14:24:55 +0600

    Update ViewreportEventController.java

--- src/MainApp/ViewreportEventController.java
+++ src/MainApp/ViewreportEventController.java
@@ -20,11 +20,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 
-/**
- * FXML Controller class
- *
- * @author Farha
- */
+
 public class ViewreportEventController implements Initializable,Serializable {
 
     @FXML
